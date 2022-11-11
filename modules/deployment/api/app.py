613f36a76409e50fdb02c9b2e25c748348972ebb
Marcus Vinicius Winograd Waddington Telles Ribeiro <marcus.waddington@radixeng.com.br>
2022-11-11 16:50:27 -0300

    Cleaned up deployment module

--- modules/deployment/api/app.py
+++ modules/deployment/api/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from data_science.modelling import SimpleModel
 import pandas as pd
 
+# load ML model to memory
 model = SimpleModel()
 model.load(model_folder="/models")
 
@@ -13,26 +14,20 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    """Process API prediction request.
+    """
+    # process request features
     request_dict = request.get_json()
-
-    print(type(request_dict))
-
-    # request_dict = {
-    #     "idx": 150000,
-    #     "features": {
-    #         "attr_a": 1,
-    #         "attr_b": "c",
-    #         "scd_a": 0.55,
-    #         "scd_b": 3
-    #     }
-    # }
-
     df_features = pd.json_normalize(request_dict["features"])
 
+    # make prediction
     predicted_label = model.predict(features = df_features)
 
-    return str(predicted_label)
+    return_dict =  {
+        "prediction": str(predicted_label)
+    }
 
+    return jsonify(return_dict)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
